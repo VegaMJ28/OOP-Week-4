8bfb181b35400a40b602aa478be7164d3a67b850
VegaMJ28 <mjusa28@gmail.com>
2026-09-18 08:32:19 -0500

    Add files via upload

---
+++ sets.guide.py
@@ -0,0 +1,19 @@
+# set = collection which is unordered, unidexed. No duplicate values.
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowl", "plate", "knife"}
+
+#utensils.add("napkin")
+#utensils.remove("fork)
+#utensils.clear()
+
+#dishes.update(utensils)
+#for x in utensils:
+    #print(x)
+
+#dinner_table = utensils.union(dishes)
+#for x in dinner_table:
+    #print(x)
+
+#print(utensils.difference(dishes))
+#print(utensils.intersection(dishes))
